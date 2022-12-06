182eb747a3c5825d5259909ebb55b7df21b42016
chebyshef <118692527+chebyshef@users.noreply.github.com>
2022-12-06 22:25:25 +0900

    Add files via upload

--- ControlExtensions/numericpadmode.js
+++ ControlExtensions/numericpadmode.js
@@ -2,7 +2,7 @@
 	const setNumericpadMode = function () {
 		var numerics = document.querySelectorAll("jqx-numeric-text-box input");
 		
-		numerics.forEach(element => element.setAttribute("inputmode", "numeric"));
+		numerics.forEach(element => element.setAttribute("inputmode", "decimal"));
 	};
 	
 	window.GWebUtils = {
